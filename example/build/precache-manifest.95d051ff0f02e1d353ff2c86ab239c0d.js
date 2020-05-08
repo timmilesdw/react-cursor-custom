e7970555f311b6974d324654a6d854c4bff364e3
Tim Miles <ifirepaille@gmail.com>
2020-05-08 17:25:04 +0500

    fixed issue in examples

---
+++ example/build/precache-manifest.95d051ff0f02e1d353ff2c86ab239c0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad89121b5651f99ae9a2b3c195227a15",
+    "url": "/react-cursor-custom/index.html"
+  },
+  {
+    "revision": "c749e905ff4b5abb4025",
+    "url": "/react-cursor-custom/static/css/main.097a9ef5.chunk.css"
+  },
+  {
+    "revision": "0f043b30f0942d792d7b",
+    "url": "/react-cursor-custom/static/js/2.e633979e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-cursor-custom/static/js/2.e633979e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c749e905ff4b5abb4025",
+    "url": "/react-cursor-custom/static/js/main.2308ac7a.chunk.js"
+  },
+  {
+    "revision": "494e4780ed5f3204e825",
+    "url": "/react-cursor-custom/static/js/runtime-main.fa1751a8.js"
+  }
+]);

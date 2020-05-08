d60fe8264714b553aeaa7a66ae824e739ceba1dd
Tim Miles <ifirepaille@gmail.com>
2020-05-08 16:19:29 +0500

    fixed some issues in examples

---
+++ docs/precache-manifest.e2218e03f7910e1c6cc532266de1d0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a574aba76fedeb6e380a1f816a3f92f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7bb450ba78d2ad95a730",
+    "url": "./static/css/main.097a9ef5.chunk.css"
+  },
+  {
+    "revision": "74dba18b927a4f5881fe",
+    "url": "./static/js/2.c427e2b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c427e2b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb450ba78d2ad95a730",
+    "url": "./static/js/main.51d32bff.chunk.js"
+  },
+  {
+    "revision": "65e98bfd168d845b4609",
+    "url": "./static/js/runtime-main.d5a50b8d.js"
+  }
+]);

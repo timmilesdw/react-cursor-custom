82846a6fa8021237543ac0d85b493c88636e422e
Tim Miles <ifirepaille@gmail.com>
2020-05-08 17:04:57 +0500

    fixed some issues in examples

--- example/build/service-worker.js
+++ example/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e2218e03f7910e1c6cc532266de1d0d3.js"
+  "/react-cursor-custom/precache-manifest.fbd7546b3fa404696cacac127289152a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cursor-custom/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

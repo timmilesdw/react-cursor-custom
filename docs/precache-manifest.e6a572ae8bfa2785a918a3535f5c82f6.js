a70dee3d9b66aad5269aea5c44772ceccd492e1d
tim <bulletcries13@gmail.com>
2020-05-10 01:57:39 +0500

    improved example + cursor wrapper

---
+++ docs/precache-manifest.e6a572ae8bfa2785a918a3535f5c82f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1f892b6656d6224700f9b4181e95ad",
+    "url": "/react-cursor-custom/index.html"
+  },
+  {
+    "revision": "e8024f8d5217c9605581",
+    "url": "/react-cursor-custom/static/css/main.bf348205.chunk.css"
+  },
+  {
+    "revision": "c48e5dd0e76459f3a71c",
+    "url": "/react-cursor-custom/static/js/2.aeebd57e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-cursor-custom/static/js/2.aeebd57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8024f8d5217c9605581",
+    "url": "/react-cursor-custom/static/js/main.8bb5a25f.chunk.js"
+  },
+  {
+    "revision": "494e4780ed5f3204e825",
+    "url": "/react-cursor-custom/static/js/runtime-main.fa1751a8.js"
+  }
+]);
